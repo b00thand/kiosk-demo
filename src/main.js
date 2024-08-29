f8284335d8d28e062dfab7dfe8743b477d9b0bd7
b00thand <ryanlucas01@outlook.com>
2024-08-29 10:18:45 -0400

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,38 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    dialNumber: 'scott.barga@coxinc.com',
+    services: [],
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+      this.services = [
+        { url: this.dialNumber, name: 'Call' },
+      ];
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english', 'norwegian'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
