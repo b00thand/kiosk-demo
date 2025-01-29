7c76e890563828947290d1f37ff9c617f281cc10
b00thand <ryanlucas01@outlook.com>
2025-01-29 10:16:27 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 function setup() {
+  let idleTimeout;
 
   Alpine.store('model', {
     currentPage: 'home', // 'home', 'service'
@@ -14,23 +15,43 @@ function setup() {
       this.services = [
         { url: this.dialNumber, name: 'Call' },
       ];
+      this.resetIdleTimer(); // Start idle timer
+      this.addIdleListeners(); // Attach event listeners
     },
+
     get page() {
       return this.currentPage;
     },
+
     set page(nextPage) {
       this.currentPage = nextPage;
+      this.resetIdleTimer(); // Reset timer on page change
     },
+
     currentLanguage: 'english',
     languages: ['english', 'norwegian'],
+    
     get language() {
       return this.currentLanguage;
     },
+
     set language(current) {
       this.currentLanguage = current;
     },
-  });
 
+    resetIdleTimer() {
+      clearTimeout(idleTimeout);
+      idleTimeout = setTimeout(() => {
+        this.currentPage = 'home'; // Return to home screen after 10s
+      }, 10000); // 10 seconds
+    },
+
+    addIdleListeners() {
+      ['click', 'mousemove', 'keypress', 'touchstart'].forEach(event => {
+        document.addEventListener(event, () => this.resetIdleTimer());
+      });
+    }
+  });
 }
 
 document.addEventListener('alpine:init', setup);
